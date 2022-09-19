623ab4dd9aaef7004e992591cedda5596a4a2735
root <root@NESTOR-MORENO-DESK.localdomain>
2022-09-18 22:36:52 -0500

    Ejercicio css

---
+++ figuras.js
@@ -0,0 +1,53 @@
+// const ladoCuadrado = 5;
+// console.log("Lado cuadrado " + ladoCuadrado);
+
+// // Cuadrado
+// function perimetroCuadrado(lado) {
+//     return lado * 4;
+//     console.log("Perímetro cuadrado " + perimetroCuadrado);
+// }
+
+// function areaCuadrado(lado ){
+//     return lado * lado;
+//     console.log("Área cuadrado " + areaCuadrado);
+// } 
+
+// // Triángulo
+
+// // Círculo
+
+
+// function calcularPerimetro() {
+//     let lado = document.getElementById("lado").value
+//     console.log( perimetroCuadrado(lado));
+// }
+
+
+
+const lista1 = [100, 200, 300, 500];
+
+let sumaLista1 = 0;
+
+
+
+// The character U+00b4 "´" could be confused with the character U+0060 "`
+
+
+function calcularMedia(lista){
+    // for (let i = 0; i < lista.length; i++) {
+    //     sumaLista1 += lista[i];
+    // }
+    // return sumaLista1/lista.length;
+    // console.log("sumaLista1:`{sumaLista1}",);
+
+    return lista.map((p, c)=>{ return p+c})/ lista.length;
+    // return lista.map((p, c)=>{ return p+c})/ lista.length;
+
+
+}
+
+
+function sumarArreglo(arr){
+
+    sumarArreglo(arr[])
+}
